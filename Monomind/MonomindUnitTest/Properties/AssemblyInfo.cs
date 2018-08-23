98dd672ec87c016629c3d6e2b5590c3664596b78
alxhu <32041622+alxhu-dev@users.noreply.github.com>
2018-08-23 20:07:49 +0200

    Basisklassen erstellt und UnitTest-Projekt erstellt

---
+++ Monomind/MonomindUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MonomindUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MIFcom GmbH")]
+[assembly: AssemblyProduct("MonomindUnitTest")]
+[assembly: AssemblyCopyright("Copyright © MIFcom GmbH 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f66dedb7-9c7f-4449-ab97-2d93693f2516")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
